b87ea27899ea74e4ef8053f7de3b529fada31fa0
Leaf <444653703@qq.com>
2022-05-19 16:22:23 +0800

    minor update

--- zqkd/zqkd_audio.js
+++ zqkd/zqkd_audio.js
@@ -10,9 +10,8 @@ taskNum 领的宝箱总数
 需要zqkdCookie，自己捉包填写，多账号@隔开，格式：
 uid=12345678&zqkey=xxxxxx&zqkey_id=yyyyyyy@uid=87654321&zqkey=zzzzzzzz&zqkey_id=aaaaaa
 
-定时:
-26 10,19 * * *
-
+cron: 26 10,19 * * *
+const $ = new Env('中青看点-听歌');
 */
 let targetTime = 8000
 let step = 360
