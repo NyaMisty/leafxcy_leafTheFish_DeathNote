8a46d2b141d494182813f07e385704e207e5853e
Misty <gyc990326@gmail.com>
2022-06-16 11:33:39 +0800

    Initial working commit

---
+++ patch_eleme.py
@@ -0,0 +1,12 @@
+import re
+import sys
+
+fn = sys.argv[1]
+with open(fn) as f:
+    content = f.read()
+
+newcontent = re.sub(r"(\['\\x69\\x64'\])=.*?\+'\\x38',(.*?)=0x3e8", r"\1='178002',\2=950", content)
+assert '178002' in newcontent or newcontent != content
+
+with open(fn, 'w') as f:
+    f.write(newcontent)
