37b772e58f736a2a4b6d54d9538df0fbfe8b6a96
leafTheFish <103831852+leafTheFish@users.noreply.github.com>
2024-04-28 23:23:14 +0800

    Update yhsh.js

--- yhsh.js
+++ yhsh.js
@@ -1,6 +1,12 @@
 /*
 永辉生活 v1.01
 
+登录前开启捉包, 捉登录包响应里面的uid和refreshToken
+把捉包或者网页登录获取的uid,refreshToken和备注(非必须)填到文件 yhshCookie.txt 里(第一次运行会自动创建), 多账号换行写
+例子: 
+123456789321654987#12345678-1345-3546-69c3-xxxxxxxxxxxx#doubi
+679113468143213246#yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy#huoge
+
 自动签到和组队互助
 目前组队互助是积分大头, 一队需要3个号, N个号可以组满N/2队
 500积分可以抵扣1元(上限貌似是抵扣20%), 小毛
